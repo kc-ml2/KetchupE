b6c5c36ccbef7de0ee1382f32da2ee57419c0ad9
ML2jinmyoung <mys1458@gmail.com>
2026-06-23 10:29:18 +0900

    [FIX] API url

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ interface Config {
 
 export const config: Config = {
   get apiUrl() {
-    return localStorage.getItem("serverUrl") || import.meta.env.VITE_API_URL || "https://mayo.ml2-alpha.com";
-  }
+    return localStorage.getItem("serverUrl") || import.meta.env.VITE_API_URL;
+  },
 };
